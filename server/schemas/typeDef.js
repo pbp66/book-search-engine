36d1118bcba8b6a90e4dc8e88f448bb68eab8ae7
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-06 19:46:15 -0500

    added model schemas to typeDefs

---
+++ server/schemas/typeDef.js
@@ -0,0 +1,44 @@
+export default typeDefs = `
+	type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+	}
+
+    type Book {
+        _id: ID!
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String!
+        link: String!
+    }
+
+    type Auth {
+        token: String!
+        user: User
+    }
+
+    input SavedBookInput {
+        authors: [String]
+        description: String!
+        title: String!
+        bookId: String!
+        image: String!
+        link: String!
+    }
+
+    type Query {
+        me(_id: ID!): User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: SavedBookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
