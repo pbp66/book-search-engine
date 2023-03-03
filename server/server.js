5491fc13119752acd0e47431c3f56a5e3fda2c78
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-03-03 18:03:51 -0500

    updated build scripts and build locations

--- server/server.js
+++ server/server.js
@@ -19,9 +19,18 @@ app.use(express.json());
 // To use with ES6, changed path.join to using URL objects:
 // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules
 if (process.env.NODE_ENV === "production") {
+	// app.use(
+	// 	express.static(new URL("../client/build", import.meta.url).pathname)
+	// );
+
 	app.use(
-		express.static(new URL("../client/build", import.meta.url).pathname)
+		express.static(new URL("./client/build", import.meta.url).pathname)
 	);
+	app.get("/", (req, res) => {
+		res.sendFile(
+			new URL("./client/build/index.html", import.meta.url).pathname
+		);
+	});
 }
 
 app.get("/", (req, res) => {
